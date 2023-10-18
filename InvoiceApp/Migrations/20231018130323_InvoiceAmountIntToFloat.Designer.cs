a9ac4a34c14f1d7279141f5574d42d47e2e4095d
Henrik <henrik.laland@bouvet.no>
2023-10-18 15:14:25 +0200

    changed invoiceAmount from int to float

--- InvoiceApp/Migrations/20231018130323_InvoiceAmountIntToFloat.Designer.cs
+++ InvoiceApp/Migrations/20231018130323_InvoiceAmountIntToFloat.Designer.cs
@@ -4,6 +4,7 @@ using InvoiceApp.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InvoiceApp.Migrations
 {
     [DbContext(typeof(InvoiceContext))]
-    partial class InvoiceContextModelSnapshot : ModelSnapshot
+    [Migration("20231018130323_InvoiceAmountIntToFloat")]
+    partial class InvoiceAmountIntToFloat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
